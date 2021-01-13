5b79131926c0529b9c00449bea3b3a92af28d0a0
Bart <manumora11@gmail.com>
2021-01-13 10:18:09 +0100

    sharedpreferences RegistFragment | falta acciones de redireccionamiento

--- app/src/main/java/com/example/toptrump/view/fragments/RegistFragment.java
+++ app/src/main/java/com/example/toptrump/view/fragments/RegistFragment.java
@@ -1,17 +1,29 @@
 package com.example.toptrump.view.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.toptrump.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegistFragment extends Fragment {
+
+    Button bt;
+    TextInputEditText texto;
+    String clave;
+    SharedPreferences sp;
 
     @Override
     public View onCreateView(
@@ -25,5 +37,35 @@ public class RegistFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        bt = view.findViewById(R.id.btEnvClav);
+        texto = view.findViewById(R.id.tietClave);
+
+
+        sp = getContext().getSharedPreferences("dato", Context.MODE_PRIVATE);
+        String comprobarClave = sp.getString("clave","");
+
+        if(!comprobarClave.isEmpty()){
+            // inicias app con clave existente
+            // te tiene que llevar a juego directamente
+        }
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!comprobarClave.isEmpty()){
+                    // cuando tu ya tienes la clave y has pulsado "administración"
+                    if(texto.getText().toString().equals(comprobarClave)){
+                        // direccionar a administración
+                    }
+                    Log.v("XYZ",comprobarClave.toString());
+                } else {
+                    clave = texto.getText().toString();
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("clave", clave);
+                    editor.commit();
+                    // lleva a administración.
+                }
+            }
+        });
     }
 }
