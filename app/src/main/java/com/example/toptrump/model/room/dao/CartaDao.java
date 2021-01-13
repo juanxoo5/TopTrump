34fe9bb3c6b340a7e579b480ed65aa94018cc15a
Bart <manumora11@gmail.com>
2021-01-13 09:01:23 +0100

    clases Repository y ViewModel hechas, falta el update

--- app/src/main/java/com/example/toptrump/model/room/dao/CartaDao.java
+++ app/src/main/java/com/example/toptrump/model/room/dao/CartaDao.java
@@ -1,11 +1,14 @@
 package com.example.toptrump.model.room.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
 import com.example.toptrump.model.room.pojo.Carta;
+
+import java.util.List;
 
 @Dao
 public interface CartaDao {
@@ -15,6 +18,9 @@ public interface CartaDao {
 
     @Query("select * from carta where id = :id")
     Carta get(long id);
+
+    @Query("select * from carta order by id")
+    LiveData<List<Carta>> getAll();
 
     @Insert
     long insert(Carta carta);
--- app/src/main/java/com/example/toptrump/model/room/dao/UsuarioDao.java
+++ app/src/main/java/com/example/toptrump/model/room/dao/UsuarioDao.java
@@ -1,5 +1,6 @@
 package com.example.toptrump.model.room.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -7,6 +8,8 @@ import androidx.room.Update;
 
 import com.example.toptrump.model.room.pojo.Carta;
 import com.example.toptrump.model.room.pojo.Usuario;
+
+import java.util.List;
 
 @Dao
 public interface UsuarioDao {
@@ -16,6 +19,9 @@ public interface UsuarioDao {
 
     @Query("select * from usuario where id = :id")
     Usuario get(long id);
+
+    @Query("select * from usuario order by id")
+    LiveData<List<Usuario>> getAll();
 
     @Insert
     long insert(Usuario usuario);
