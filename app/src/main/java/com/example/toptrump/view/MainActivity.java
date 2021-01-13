99d3eb6df701f2becc4067b9f23f396cbbcfa3b3
JoshSant <josesantgarc@gmail.com>
2021-01-13 09:11:00 +0100

    main

--- app/src/main/java/com/example/toptrump/view/MainActivity.java
+++ app/src/main/java/com/example/toptrump/view/MainActivity.java
@@ -40,16 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_PHONE_STATE);
             }
         }
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        fab.hide();
     }
 
     @Override
