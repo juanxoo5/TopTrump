a0bf8c52312dce1ff3c140ff51b5886c90d99362
JoshSant <josesantgarc@gmail.com>
2021-01-20 09:10:29 +0100

    toolbar arreglada

--- app/src/main/java/com/example/toptrump/view/fragments/AdmUsuaFragment.java
+++ app/src/main/java/com/example/toptrump/view/fragments/AdmUsuaFragment.java
@@ -28,6 +28,7 @@ public class AdmUsuaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_adm_user, container, false);
     }
 
