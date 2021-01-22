cca8cf200bbf3733603d08cbfec301cfc1f3bd75
Bart <manumora11@gmail.com>
2021-01-22 09:49:38 +0100

    Añadidos avatares

--- app/src/main/java/com/example/toptrump/model/Broadcast.java
+++ app/src/main/java/com/example/toptrump/model/Broadcast.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,7 +14,14 @@ import com.google.android.material.snackbar.Snackbar;
 public class Broadcast extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, ifilter);
 
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level * 100 / (float)scale;
+        Log.v("XYZ",level + " | " + scale + " | " + batteryPct);
         Toast.makeText(context, "Top Trump - Batería baja, por favor conecte el cargador", Toast.LENGTH_LONG).show();
         Log.v("XYZ", "BATTERY LOW!!");
 
