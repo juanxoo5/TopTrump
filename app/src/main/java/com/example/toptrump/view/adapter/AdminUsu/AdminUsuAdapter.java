9e204873db5f24c49713dabe62199895346d9d56
JoshSant <josesantgarc@gmail.com>
2021-01-22 09:22:46 +0100

    cambios adapters

---
+++ app/src/main/java/com/example/toptrump/view/adapter/AdminUsu/AdminUsuAdapter.java
@@ -0,0 +1,42 @@
+package com.example.toptrump.view.adapter.AdminUsu;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+import com.example.toptrump.model.room.pojo.Usuario;
+
+public class AdminUsuAdapter extends ListAdapter<Usuario, AdminUsuViewHolder> {
+
+    public AdminUsuAdapter( @NonNull DiffUtil.ItemCallback<Usuario> diffCallback) {
+        super(diffCallback);
+    }
+
+    @NonNull
+    @Override
+    public AdminUsuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return AdminUsuViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdminUsuViewHolder holder, int position) {
+        Usuario current = getItem(position);
+        holder.bind(current.getNombre());
+    }
+
+    public static class UsuarioDiff extends DiffUtil.ItemCallback<Usuario> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Usuario oldItem, @NonNull Usuario newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Usuario oldItem, @NonNull Usuario newItem) {
+            return oldItem.getNombre().equals(newItem.getNombre());
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/toptrump/view/adapter/AdminUsu/AdminUsuViewHolder.java
@@ -0,0 +1,33 @@
+package com.example.toptrump.view.adapter.AdminUsu;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.toptrump.R;
+
+public class AdminUsuViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView tvNombUsu;
+
+    public AdminUsuViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.tvNombUsu = itemView.findViewById(R.id.tvNombUsu);
+    }
+
+    @SuppressLint("ResourceType")
+    public void bind(String text) {
+        tvNombUsu.setText(text);
+    }
+    public static AdminUsuViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_usuario, parent, false);
+        return new AdminUsuViewHolder(view);
+    }
+
+}
